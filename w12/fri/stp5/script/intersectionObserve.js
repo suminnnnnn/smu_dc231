c1d633c6988534848d9e1c6dd3ea09db98dc9d1b
suminnnnnn <126849506+suminnnnnn@users.noreply.github.com@email.com>
2023-06-02 01:36:24 +0900

    12주차 영상보고 따라하는 중..

---
+++ w12/fri/stp5/script/intersectionObserve.js
@@ -0,0 +1,46 @@
+let observerOption = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.75,
+};
+
+//function observerCallback() {}
+let scrollFunction = (observingTargets) => {
+  observingTargets.ForEach(
+    (eachObservingTarget) => {
+      if (eachObservingTarget.isIntersecting) {
+        eachObservingTarget.target.classList.add(
+          "triggered"
+        );
+      } else {
+        eachObservingTarget.target.classList.remove(
+          "triggered"
+        );
+      }
+    }
+  );
+};
+
+let observer = new IntersectionObserver(
+  observerCallback,
+  observerOption
+);
+
+let elems = document.querySelevtorAll(
+  ".animTrigger"
+);
+console.log(elems);
+// // 하드코딩
+// observer.observer(elems[0]);
+// observer.observer(elems[0]);
+// observer.observer(elems[0]);
+// observer.observer(elems[0]);
+// ...
+// // 조금 똑똑한 코딩
+// for (left idx = 0; idx < elems.lenght; idx++) {
+// observer.observe(elems[idx]);
+// }
+// // 있어보이는 토딩
+elems.forEach((eachElem) => {
+  observer.observe(eachElem);
+});
